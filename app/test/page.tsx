30f2c3e13a25263145a66fc5d5f14d9d16154d8b
Niek <frankenstein795@gmail.com>
2023-10-23 14:23:29 +0200

    Initial nextjs prototype counter app

---
+++ app/test/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {decrement, reset} from "@/app/Slices";
+import {RootState} from "@/app/store";
+import Link from 'next/link';
+export default function Home() {
+  const counter = useSelector((state: RootState) => state.Counter)
+  const dispatch = useDispatch()
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl">Hello World</h1>
+      <div className="text-center">
+        <h2>Your count is:</h2>
+        <h2 className="text-7xl">{counter}</h2>
+      </div>
+      <div className="flex flex-col gap-4">
+        <button onClick={() => dispatch(decrement())} className="border-white border-4 w-64 h-12">Decrement</button>
+        <button onClick={() => dispatch(reset())} className="border-white border-4 w-64 h-12">Reset</button>
+        <Link href="/" className="border-white border-4 w-64 h-12 flex place-items-center justify-center"><h1>Next Page</h1></Link>
+      </div>
+    </main>
+    )
+}
